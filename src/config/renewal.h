7a4a8982663ff5e9ff66137ea1f557d57e743989
Sam <johnsamuel_santos3@yahoo.com>
2013-06-15 10:31:32 +0800

    Updated Hercules Removed Renewal Drop and Exp Rate Removed unused NPCs

--- src/config/renewal.h
+++ src/config/renewal.h
@@ -35,14 +35,14 @@
 /// leave this line to enable renewal item drop rate algorithms
 /// while enabled a special modified based on the difference between the player and monster level is applied
 /// based on the http://irowiki.org/wiki/Drop_System#Level_Factor table
-#define RENEWAL_DROP
+//#define RENEWAL_DROP
 
 /// renewal exp rate algorithms
 /// (disable by commenting the line)
 ///
 /// leave this line to enable renewal item exp rate algorithms
 /// while enabled a special modified based on the difference between the player and monster level is applied
-#define RENEWAL_EXP
+//#define RENEWAL_EXP
 
 /// renewal level modifier on damage
 /// (disable by commenting the line)

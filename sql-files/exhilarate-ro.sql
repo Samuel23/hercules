1f367d3827cccf0916c0ee1bbf7ae9eeea827c6b
Sam <johnsamuel_santos3@yahoo.com>
2013-06-26 11:53:54 +0800

    Added Itemizer NPC Added Coin Exchanger in the All in One NPC Added itemizer.sql into exhilarate-ro.sql

--- sql-files/exhilarate-ro.sql
+++ sql-files/exhilarate-ro.sql
@@ -833,3 +833,24 @@ CREATE TABLE IF NOT EXISTS `mvp_ranking` (
 `emp_kills` smallint(6) unsigned NOT NULL default '0',
 PRIMARY KEY (`char_id`)
 ) ENGINE=MyISAM;
+
+--
+-- Itemizer
+--
+
+DROP TABLE IF EXISTS `itemizer`;
+CREATE TABLE IF NOT EXISTS `itemizer` (
+  `id` int(11) unsigned NOT NULL auto_increment,
+  `item_id` int(11) NOT NULL,
+  `item_amount` int(11) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `bound` int(11) NOT NULL,
+  `char_id` int(11) NOT NULL,
+  `char_name` varchar(255) NOT NULL,
+  `reason` varchar(255) NOT NULL,
+  `by_gm` varchar(255) NOT NULL,
+  `when` datetime NOT NULL,
+  `for` varchar(255) NOT NULL,
+  `collected` tinyint(1) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM ;
